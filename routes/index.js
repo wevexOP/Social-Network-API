04aaecdb268e8fabc923361267b26b8ee02282d4
Dominic <dompaulazzo@gmail.com>
2023-02-17 14:26:39 -0800

    tweaks

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 router.use((req, res) => {
-  res.status(404).send("<h1>😝 404 Error!</h1>");
+  res.status(404).send("<h1>404 Error. Page not found. Perhaps it was never suppose to be found?</h1>");
 });
 
 module.exports = router;
